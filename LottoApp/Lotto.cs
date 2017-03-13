7d315b60d3fa0c7fc78af23494d22667df56ad7a
Katri <Katri@DESKTOP>
2017-03-13 23:41:11 +0200

    not finished

---
+++ LottoApp/Lotto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LottoApp
+{
+    class Lotto
+    {
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+        public int num3 { get; set; }
+        public int num4 { get; set; }
+        public int num5 { get; set; }
+        public int num6 { get; set; }
+        public int num7 { get; set; }
+    }
+}
---
+++ LottoApp/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace LottoApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void drawButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            // random number from range 1-39
+            Random rand = new Random();
+            int num1 = rand.Next(1, 39 + 1);
+            int num2 = rand.Next(1, 39 + 1);
+            int num3 = rand.Next(1, 39 + 1);
+            int num4 = rand.Next(1, 39 + 1);
+            int num5 = rand.Next(1, 39 + 1);
+            int num6 = rand.Next(1, 39 + 1);
+            int num7 = rand.Next(1, 39 + 1);
+
+
+          /*  int number1 = num1;
+            int number2 = num2;*/
+
+            randNumTextBlock.Text = num1.ToString() + " " + num2.ToString() + " " + num3.ToString() + " " + num4.ToString() + " "
+                + num5.ToString() + " " + num6.ToString() + " " + num7.ToString();
+
+        }
+
+    }
+}
