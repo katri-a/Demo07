7a5426f37a9fc6ac0b3c2d7047340fc0c6c43a6c
Katri <Katri@DESKTOP>
2017-03-15 19:31:36 +0200

    done

--- LottoApp/Lotto.cs
+++ LottoApp/Lotto.cs
@@ -8,12 +8,42 @@ namespace LottoApp
 {
     class Lotto
     {
-        public int num1 { get; set; }
-        public int num2 { get; set; }
-        public int num3 { get; set; }
-        public int num4 { get; set; }
-        public int num5 { get; set; }
-        public int num6 { get; set; }
-        public int num7 { get; set; }
+        private Random rand = new Random();
+        private int Number;
+        private List<int> Numbers = new List<int>();
+        private List<string> NumbersString = new List<string>();
+        private List<string> Rows = new List<string>();
+
+        public string GetRows(int NumberOfRows)
+        {
+            for (int i = 0; i < NumberOfRows; i++)
+            {
+                while (Numbers.Count < 7)
+                {
+                    Number = rand.Next(1, 39 + 1);
+                    if (!Numbers.Contains(Number)) //jos numero ei ole jo listassa, se lisätään sinne
+                    {
+                        Numbers.Add(Number);
+                    }
+
+                }
+                for (int j = 0; j < 7; j++)
+                {
+                    if (Numbers[j] < 10) //numerot 2 merkin pituisia jotta tulee suorat lottorivit
+                    {
+                        NumbersString.Add("0" + Numbers[j].ToString());
+                    }
+                    else
+                    {
+                        NumbersString.Add(Numbers[j].ToString());
+                    }
+                }
+                NumbersString.Sort(); //arvotut luvut suuruusjärjestykseen
+                Rows.Add(string.Join(" ", NumbersString));
+                Numbers.Clear();
+                NumbersString.Clear();
+            }
+            return string.Join(Environment.NewLine, Rows);
+        }
     }
 }
--- LottoApp/MainPage.xaml.cs
+++ LottoApp/MainPage.xaml.cs
@@ -29,24 +29,27 @@ namespace LottoApp
 
         private void drawButton_Click(object sender, RoutedEventArgs e)
         {
+            int RowsAmount;
+            try
+            {
+                RowsAmount = int.Parse(drawTextBox.Text);
+            } catch
+            {
+                RowsAmount = 0;
+            }
+            if (RowsAmount != 0)
+            {
+                // olioioio Lotto-luokasta
+                Lotto Arvonta = new Lotto();
+                string Rows = Arvonta.GetRows(RowsAmount);
+                randNumTextBlock.Text += Rows + System.Environment.NewLine;
+            }
+        }
 
-            // random number from range 1-39
-            Random rand = new Random();
-            int num1 = rand.Next(1, 39 + 1);
-            int num2 = rand.Next(1, 39 + 1);
-            int num3 = rand.Next(1, 39 + 1);
-            int num4 = rand.Next(1, 39 + 1);
-            int num5 = rand.Next(1, 39 + 1);
-            int num6 = rand.Next(1, 39 + 1);
-            int num7 = rand.Next(1, 39 + 1);
-
-
-          /*  int number1 = num1;
-            int number2 = num2;*/
-
-            randNumTextBlock.Text = num1.ToString() + " " + num2.ToString() + " " + num3.ToString() + " " + num4.ToString() + " "
-                + num5.ToString() + " " + num6.ToString() + " " + num7.ToString();
-
+        //clear button
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            randNumTextBlock.Text = "";
         }
 
     }
